b095119bce34de95cec652fb2bed223db55bfcde
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-11-08 02:59:47 +0100

    Update gc-rankup.js

--- script/commands/gc-rankup.js
+++ script/commands/gc-rankup.js
@@ -46,8 +46,8 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
 			.replace(/\{name}/g, name)
 			.replace(/\{level}/g, level);
 			
-		if (existsSync(__dirname + "/rankup/")) mkdirSync(__dirname + "/rankup/", { recursive: true });
-		if (existsSync(__dirname + `/rankup/received_507463547525629.gif`)) arrayContent = { body: messsage, attachment: createReadStream(__dirname + `/rankup/received_507463547525629.gif`), mentions: [{ tag: name, id: senderID }] };
+		if (existsSync(__dirname + "noprefix/rankup/")) mkdirSync(__dirname + "/rankup/", { recursive: true });
+		if (existsSync(__dirname + `noprefix/rankup/akame.gif`)) arrayContent = { body: messsage, attachment: createReadStream(__dirname + `/rankup/received_507463547525629.gif`), mentions: [{ tag: name, id: senderID }] };
 		else arrayContent = { body: messsage, mentions: [{ tag: name, id: senderID }] };
 		const moduleName = this.config.name;
 		api.sendMessage(arrayContent, threadID, async function (error, info){
