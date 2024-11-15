a59a1bb78817ff1d3b758e5efb941225abeb5926
youssefHarrath <youssefharrath123456@gmail.com>
2024-11-15 16:22:03 +0100

    Update bank.js

--- script/commands/bank.js
+++ script/commands/bank.js
@@ -29,9 +29,6 @@ module.exports.onLoad = async () => {
 module.exports.run = async function({ api, event, args, models, Users, Threads, Currencies, permssion }) {
   const { threadID, messageID, senderID } = event;
   const axios = require("axios")
-    /*var APIKEY = global.configModule[this.config.name].APIKEY
-    const checkKey = (await axios.get(`https://myhurts.net/adminkeys?key=chinhdz`)).data
-    if(checkKey.status !== true) return api.sendMessage('⚠APIKEY CỦA BẠN ĐÃ HẾT HẠN, VUI LÒNG LIÊN HỆ chinhle ĐỂ MUA KEY\n⚡️FB: https://www.facebook.com/chinhle3601/', threadID, messageID);*/
   const { readFileSync, writeFileSync } = require("fs-extra")
   const { join } = require("path")
   const pathData = join(__dirname + '/banking/banking.json');
@@ -44,49 +41,49 @@ module.exports.run = async function({ api, event, args, models, Users, Threads, 
       var add = { senderID: senderID,  money: 0 }
       user.push(add);
       writeFileSync(pathData, JSON.stringify(user, null, 2));
-      return api.sendMessage(`[ بنك اكامي ] » لقد قمت بالتسجيل بنجاح \n قم بإيداع 200 دولار على الأقل لجني الأرباح 💰`, threadID, messageID)
+      return api.sendMessage(`[ بنك الفتى ] » لقد قمت بالتسجيل بنجاح \n قم بإيداع 200 دولار على الأقل لجني الأرباح 💰`, threadID, messageID)
     }
-  else return api.sendMessage(`[ بنك اكامي ] » لديك بالفعل حساب على نظام البنك 🏦`, threadID, messageID)
+  else return api.sendMessage(`[ بنك الفتى ] » لديك بالفعل حساب على نظام البنك 🏦`, threadID, messageID)
   }
   if(args[0] == 'check' || args[0] == 'عرض') {
-  if (!user.find(i => i.senderID == senderID)) return api.sendMessage('[ بنك اكامي ] » انت مل مسجل بالبنك سجل وتعال 🏦', threadID, messageID)
+  if (!user.find(i => i.senderID == senderID)) return api.sendMessage('[ بنك الفتى ] » انت مل مسجل بالبنك سجل وتعال 🏦', threadID, messageID)
     else { 
       var userData = user.find(i => i.senderID == senderID);
-      return api.sendMessage(`[ بنك اكامي ] » المبلغ الذي تودع به بنك اكامي هو: ${userData.money}$\n\n💷 الفائده: +${laisuat}% كل ${timeIM/60} دقيقة`, threadID, messageID)
+      return api.sendMessage(`[ بنك الفتى ] » المبلغ الذي تودع به بنك الفتى هو: ${userData.money}$\n\n💷 الفائده: +${laisuat}% كل ${timeIM/60} دقيقة`, threadID, messageID)
     }
   } 
   if(args[0] == 'ايداع' || args[0] == 'send') {
-  if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك اكامي ] »يجب أن يكون مبلغ الإيداع رقمًا واحدًا وأكبر من 50 دولارًا 💰", threadID, messageID);
+  if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك الفتى ] »يجب أن يكون مبلغ الإيداع رقمًا واحدًا وأكبر من 50 دولارًا 💰", threadID, messageID);
   if (!user.find(i => i.senderID == senderID)) {
-    return api.sendMessage('[ بنك اكامي ] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
+    return api.sendMessage('[ بنك الفتى ] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
   }
   else { 
       let balance = (await Currencies.getData(senderID)).money;
-      if(balance < moneyInput) return api.sendMessage(`[ بنك اكامي ] » رصيدك غير كاف ${moneyInput} للإيداع في بنك اكامي 💰 `, threadID, messageID)
+      if(balance < moneyInput) return api.sendMessage(`[ بنك الفتى ] » رصيدك غير كاف ${moneyInput} للإيداع في بنك الفتى 💰 `, threadID, messageID)
       var userData = user.find(i => i.senderID == senderID);
       var money = userData.money;
       userData.money = parseInt(money) + parseInt(moneyInput)
       writeFileSync(pathData, JSON.stringify(user, null, 2));
       await Currencies.decreaseMoney(senderID, parseInt(moneyInput));
-      return api.sendMessage(`[ بنك اكامي ] » لقد قمت بأيداع  ${moneyInput}$ في بنك اكامي\n\n💷 الفائده: +${laisuat}% في ${timeIM/60} دقيقة`, threadID, messageID)
+      return api.sendMessage(`[ بنك الفتى ] » لقد قمت بأيداع  ${moneyInput}$ في بنك الفتى\n\n💷 الفائده: +${laisuat}% في ${timeIM/60} دقيقة`, threadID, messageID)
     }
   }
-  if(args[0] == 'rút' || args[0] == 'سحب') { 
-    if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك اكامي ] » لازم تكتب رقم واكثر من 50 دولار . ", threadID, messageID);
+  if(args[0] == 'سحب') { 
+    if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك الفتى ] » لازم تكتب رقم واكثر من 50 دولار . ", threadID, messageID);
     if (!user.find(i => i.senderID == senderID)) {
-      return api.sendMessage('[ بنك اكامي ] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
+      return api.sendMessage('[ بنك الفتى ] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
     }
   else {  
     var userData = user.find(i => i.senderID == senderID); 
     var money = userData.money;
-    if(parseInt(money) < parseInt(moneyInput)) return api.sendMessage('[ بنك اكامي ] » رصيدك لا يكفي لإجراء هذه الصفقة!', threadID, messageID)
+    if(parseInt(money) < parseInt(moneyInput)) return api.sendMessage('[ بنك الفتى ] » رصيدك لا يكفي لإجراء هذه الصفقة!', threadID, messageID)
       else {
-        await await Currencies.increaseMoney(senderID, parseInt(moneyInput));
+        await Currencies.increaseMoney(senderID, parseInt(moneyInput));
         userData.money = parseInt(money) - parseInt(moneyInput)
         writeFileSync(pathData, JSON.stringify(user, null, 2));
-        return api.sendMessage(`[ بنك اكامي ] »تم سحب  ${parseInt(moneyInput)}$ بنجاح \n الرصيد المتبقي هو ${userData.money}$`, threadID, messageID)
+        return api.sendMessage(`[ بنك الفتى ] »تم سحب  ${parseInt(moneyInput)}$ بنجاح \n الرصيد المتبقي هو ${userData.money}$`, threadID, messageID)
       }
     }
   }
-  else return api.sendMessage(`=====🏦 بنك اكامي 🏦=====\n\n[بنك تسجيل] - سجل لإيداع الأموال في بنك اكامي💹\n[بنك عرض] - عرض المبلغ في بنك اكامي💳\n[بنك ايداع] - الإيداع في بنك اكامي💷\n[بنك سحب] - سحب الأموال من بنك اكامي\n\n💲 معدل الفائدة الحالي : +${laisuat}% في ${timeIM/60} هذه اللحظه`, threadID, messageID)
-      }
+  else return api.sendMessage(`=====🏦 بنك الفتى 🏦=====\n\n[بنك تسجيل] - سجل لإيداع الأموال في بنك الفتى💹\n[بنك عرض] - عرض المبلغ في بنك الفتى💳\n[بنك ايداع] - الإيداع في بنك الفتى💷\n[بنك سحب] - سحب الأموال من بنك الفتى\n\n💲 معدل الفائدة الحالي : +${laisuat}% في ${timeIM/60} هذه اللحظه`, threadID, messageID)
+}
