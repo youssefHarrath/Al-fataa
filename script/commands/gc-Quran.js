ed8bc2ca985b91b70c317d4bf7a5328463eec070
youssefHarrath <youssefharrath123456@gmail.com>
2024-11-15 16:03:44 +0100

    Update gc-Quran.js

--- script/commands/gc-Quran.js
+++ script/commands/gc-Quran.js
@@ -1,10 +1,10 @@
- module.exports.config = {
+module.exports.config = {
   name: "قرآن",
   version: "1.0.0",
   hasPermssion: 0,
-   credits: "S H A D O W",
+  credits: "S H A D O W",
   description: "أيات قرأنية",
-   commandCategory: "الــــجـــروب", 
+  commandCategory: "الــــجـــروب", 
   usages: " ",
   cooldowns: 5,
   dependencies: {
@@ -15,11 +15,11 @@
 };
 
 module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
+  const axios = global.nodemodule["axios"];
+  const request = global.nodemodule["request"];
+  const fs = global.nodemodule["fs-extra"];
   var link = [
-"https://audio.jukehost.co.uk/Rv4gPOzornuPEGv5XIJWNm56CWG3fnSp",
+    "https://audio.jukehost.co.uk/Rv4gPOzornuPEGv5XIJWNm56CWG3fnSp",
     "https://audio.jukehost.co.uk/e5st7AgyjXleDzPUwBvRt70GNaKpQwNV",
     "https://audio.jukehost.co.uk/ngWtCumV4MTVPBLyk14O9ev2ZxT0sEzr",
     "https://audio.jukehost.co.uk/jnnKiNtQLdsRiorMbHGgm0dLWy64YYx4",
@@ -45,15 +45,9 @@ const fs = global.nodemodule["fs-extra"];
     "https://audio.jukehost.co.uk/KIqlgdinCb9J6uZzOP7hLGsjYlxi7hg8",
     "https://audio.jukehost.co.uk/Pif1PGNAqBWgClf67N2r4zD5Llij2SrV",
   ];
-	 var max = Math.floor(Math.random() * 6);  
-var min = Math.floor(Math.random() * 2);
-  var data = await Currencies.getData(event.senderID);
-  var exp =  data.exp;
-  var money = data.money
-      if(money < 100) api.sendMessage(" - يجب أن يكون لديك على الاقل 100 دولار للأستماع للأيات القرأنية ! ",event.threadID,event.messageID)
-          else {
-   Currencies.setData(event.senderID, options = {money: money - 100})
-   var callback = () => api.sendMessage({body:` `,attachment: fs.createReadStream(__dirname + "/cache//288.mp3")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/288.mp3"), event.messageID); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/288.mp3")).on("close",() => callback());
-     }
-   };
+  var max = Math.floor(Math.random() * 6);  
+  var min = Math.floor(Math.random() * 2);
+  
+  var callback = () => api.sendMessage({body:` `,attachment: fs.createReadStream(__dirname + "/cache//288.mp3")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/288.mp3"), event.messageID); 
+  return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/288.mp3")).on("close",() => callback());
+};
